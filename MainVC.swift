5985d0c0dcbefd6236ef62ac8d21b6fc6de471ae
Florentino Velasco Salgado <florentino.velasco.salgado@gmail.com>
2016-02-13 14:08:15 +0100

    Inclusión de nuevos controladores para las vistas

---
+++ MainVC.swift
@@ -0,0 +1,27 @@
+//
+//  MainVC.swift
+//  desformateador
+//
+//  Created by Florentino Velasco Salgado on 13/2/16.
+//  Copyright © 2016 Florentino Velasco Salgado. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+
+}
